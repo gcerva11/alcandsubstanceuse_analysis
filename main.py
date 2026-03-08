5dde5c17b9126569f18b052783219e65889a8565
Angel <99159538+GameHeadsVyse@users.noreply.github.com>
2026-03-08 14:34:16 -0700

    minor changes to main.py

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 from data.dictionary import data, count
-#inside the main class lets only havbe the fucntions that were going to use for analysis
-#i have the functions ive made so far for data reorganization insdie the dictionary.py
+from classdefinitions import UsageRecord
 
-#for out analusis, i think we could do
+#inside the main class lets only have the functions that were going to use for analysis
+#i have the functions ive made so far for data reorganization inside the dictionary.py
+
+#for out analysis, I think we could do
 
 #upper vs underclassmen percentage of use frequency, and what are their implications
+    #ah alright, I made seperate scripts_ignore for all the functions but I'll just write here instead.
+
+#The dictionary would be our input then right?
+
+def alcohol_freq(UsageRecord) -> float: #Maybe our output is a sentence with a float for percentage?
+
+
+def substance_freq(UsageRecord) -> float:  # Maybe our output is a sentence with a float for percentage?
 
 #upper vs under high_risk frequency
 
-#usage diferences of different substances
+def high_risk_frq(UsageRecord) -> float:
+
+#usage differences of different substances
+
+def substance_differences(UsageRecord) -> float:
 
 #idk what else
 
-#and then question 22L1-12 is like about consequences, we could analys a litle bit og that
-#it wuld be a but dufferbet tho bc its not explicitly saying if they are upper or underclassmen
+#and then question 22L1-12 is like about consequences, we could analys a little bit on that
+#it would be a bit different tho bc it's not explicitly saying if they are upper or underclassmen
+
+#Maybe we can show the data on students contemplating dropping out or having worse grades?
+#Honestly the data wouldn't be able to show too much without suggesting connections through our own interpretation of that data
