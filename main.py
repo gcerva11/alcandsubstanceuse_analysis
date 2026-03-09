387d8e79fbee5ca160a5fd7e0c0ac9fa635d0a14
Angel <99159538+GameHeadsVyse@users.noreply.github.com>
2026-03-08 21:49:33 -0700

    more output text

--- main.py
+++ main.py
@@ -47,14 +47,20 @@ def analysis_exec() -> None:
                 "There may be errors or oversights in any connections made due to the limited time and scope involved in development.\n\n\n"
                    "STUDENT DEMOGRAPHICS\n"
                    ""
-                   "ALCOHOL/SUBSTANCE USAGE \n"
+                   "ALCOHOL/SUBSTANCE USAGE\n"
                    "The average amount of alcohol usage overall is "f"{avg_alcohol_usage()}"" throughout the survey.\n" #insert into blank
                    ""
                    "The average amount of substance usage overall is "f"{avg_substance_usage()}"" throughout the survey.\n"
                    ""
-                   "The difference between average alcohol usage and average substance usage is "f"{}"" between ___\n\n" #if we want to compare them
-                   "ACADEMIC STATS"
+                   "The difference between average alcohol usage and average substance usage is "f"{alc_substance_diff()}"" [units]\n\n" 
+                   #if we want to compare them
+                   "ACADEMIC STATS\n"
+                   "Underclassmen (1st & 2nd Years) report having an average GPA of "f"{avg_und_class_gpa()}"" in the survey compared to "
+                   "Upperclassmen" "(3rd & 4th years) reporting GPA averages of "f"{avg_up_class_gpa()}"""
                    ""
                    "\n\n"
-                   "MENTAL HEALTH STATS")
+                   "MENTAL HEALTH STATS\n"
+                   "The average amount of sleep students have is around "f"{avg_sleep()}"""
+                   ""
+                   "")
 analysis_exec()
